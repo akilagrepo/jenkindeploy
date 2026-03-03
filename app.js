f01a2b833afe7794046945096f3ddf986dbbe4c6
Akila Pradeep <akilaag@gmail.com>
2026-03-03 12:33:30 +0530

    chore: Testing with the message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("🚀 DevOps Demo App Deployed via Jenkins - testing Akila Pradeep!");
+  res.send("🚀 DevOps Demo App Deployed via Jenkins - testing Akila GP!");
 });
 
 app.listen(3000, () => {
